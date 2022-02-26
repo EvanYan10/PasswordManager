dbb2b3475b571ee46c0fe6c33257f5128c6ef87e
EvanYan10 <yf.yan@icloud.com>
2022-02-26 18:06:27 +0800

    Update pswdmgr.py

--- pswdmgr.py
+++ pswdmgr.py
@@ -4,21 +4,24 @@ def main():
 2) add account / password
 0) exit."""
 
-    print(menu)
-
     while True:
+        print(menu)
         n = int(input())
         if n == 1:
+            print("Please input the key.")
+            key = input()
             print("Evan 123456")
-            break
         elif n == 2:
+            print("Please input the account.")
             a = input()
+            print("Please input the password.")
             p = input()
             print(a, p)
-            break
         elif n == 0:
             print("byebye")
             break
+        else:
+            print("input error")
 
 if __name__ == '__main__':
     main()
