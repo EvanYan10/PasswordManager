0013909470dc93230651acbf72929635f96f041f
YanWei <theoneyw@gmail.com>
2022-02-26 18:01:58 +0800

    simplify the menu

--- pswdmgr.py
+++ pswdmgr.py
@@ -1,28 +1,24 @@
 def main():
-    print("Please choose what you want to do. 1 for querying the passwords. 2 for adding passwords. 0 for exit.")
-    n = int(input())
-    if n == 1:
-        print("Evan 123456")
-    elif n == 2:
-        a = input()
-        p = input()
-        print(a, p)
-    elif n == 0:
-        print("byebye")
-    else:
-        while True:
-            n = int(input())
-            if n == 1:
-                print("Evan 123456")
-                break
-            elif n == 2:
-                a = input()
-                p = input()
-                print(a, p)
-                break
-            elif n == 0:
-                print("byebye")
-                break
+    menu = """Please choose what you want to do:
+1) query account / password
+2) add account / password
+0) exit."""
+
+    print(menu)
+
+    while True:
+        n = int(input())
+        if n == 1:
+            print("Evan 123456")
+            break
+        elif n == 2:
+            a = input()
+            p = input()
+            print(a, p)
+            break
+        elif n == 0:
+            print("byebye")
+            break
 
 if __name__ == '__main__':
     main()
