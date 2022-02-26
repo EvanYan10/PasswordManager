2270f2b5872122de613236c178dee0c6daf24ba4
EvanYan10 <yf.yan@icloud.com>
2022-02-26 18:24:00 +0800

    Update pswdmgr.py

--- pswdmgr.py
+++ pswdmgr.py
@@ -11,19 +11,37 @@ def main():
         n = int(input())
         os.system('cls')
         if n == 1:
-            print("Your status: QUERYING")
+            print("Your status: QUERY")
             print("Please input the key.")
             key = input()
             print("Your account is: Evan \nYour password is: 123456")
         elif n == 2:
-            print("Your status: ADDING")
+            print("Your status: ADD")
             print("Please input the key.")
-            k = input()
+            while True:
+                k1 = input()
+                k2 = input()
+                if k1 == k2:
+                    break
+                os.system('cls')
+                print("Please input the same keys.")
             print("Please input the account.")
-            a = input()
+            while True:
+                a1 = input()
+                a2 = input()
+                if a1 == a2:
+                    break
+                os.system('cls')
+                print("Please input the same accounts.")
             print("Please input the password.")
-            p = input()
-            print("Your key is:", k, "Your account is:", a,"Your password is:", p)
+            while True:
+                p1 = input()
+                p2 = input()
+                if p1 == p2:
+                    break
+                os.system('cls')
+                print("Please input the same passwords.")
+            print("Your key is:", k1, "Your account is:", a1,"Your password is:", p1)
         elif n == 0:
             print("byebye")
             break
